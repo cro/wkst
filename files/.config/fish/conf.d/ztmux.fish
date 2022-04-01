8999808283383a1a7aa0e4a311859010cd57c337
C. R. Oldham <cro@ncbt.org>
2022-04-01 13:00:52 -0600

    Zellij config

--- files/.config/fish/conf.d/ztmux.fish
+++ files/.config/fish/conf.d/ztmux.fish
@@ -2,11 +2,7 @@ if status --is-login
   if test (uname) = "Darwin"; or test (uname) = "Linux"
     set PPID (ps -p %self -o ppid | tail -1 | xargs)
     if not test (ps -p $PPID | grep ssh > /dev/null); and not set -q ZELLIJ
-      if test (zellij list-sessions > /dev/null 2>&1)
-        zellij attach; and exec true
-      else
-        zellij; and exec true
-      end
+      zellij attach --create; and exec true
       echo "zellij failed to start; using plain fish shell"
     end
   end
