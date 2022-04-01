3222b6a9eb182ce0d9b1e9197f975ba6366bcd39
C. R. Oldham <cro@ncbt.org>
2022-04-01 12:29:24 -0600

    Better linux config

--- files/.config/fish/conf.d/config.Linux.fish
+++ files/.config/fish/conf.d/config.Linux.fish
@@ -2,7 +2,7 @@ if test "Linux" = (uname)
 
   set -gx EDITOR lvim
 
-  if status --is-interactive and ! which keychain > /dev/null
+  if status --is-interactive; and ! which keychain > /dev/null
     keychain --eval --quiet --inherit any -Q id_rsa id_ed25519 | source
   end
 
